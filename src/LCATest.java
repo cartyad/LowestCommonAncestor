8d56d653b1849bd5a242f8605f685efcd5ebebcc
cartyad <cartyad@tcd.ie>
2020-10-05 23:29:59 +0100

    Update LCATest.java
    Test the construction of a tree with only 1 node where root value is 0

--- src/LCATest.java
+++ src/LCATest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,11 +8,18 @@ class LCATest {
 	LCA tree = new LCA();
 
 	@Test
-	public void testNullTree() 
+	public void testNullTreeError() 
 	{
 		assertNull(tree.root);
 	}
 	
-	
+	@Test
+	public void testTreeConstructor() {
+		//Create a tree with only one single node, the root
+		Node rootNode = new Node(0);
+		assertEquals("Root Value is Zero",0 ,rootNode.value);
+		assertEquals("Only node is root so null is expected",null ,rootNode.right);
+		assertEquals("Only node is root so null is expected",null ,rootNode.left);
+	}
 
 }
