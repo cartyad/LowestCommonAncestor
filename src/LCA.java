3abc8ac964c2913f163e9fa55642b314f1ee3326
cartyad <cartyad@tcd.ie>
2020-10-05 20:24:06 +0100

    Update LCA.java
    Initialised new findPath method which will handle error inputs and find the correct path for the existing nodes else return false which will inturn return -1 throughout to be flagged as an error

--- src/LCA.java
+++ src/LCA.java
@@ -16,6 +16,13 @@ public class LCA {
 	
 	private int findLCA(Node root, int v, int w)
 	{
+		if (!findPath(root, v, path1) || !findPath(root, w, path2)) {
+			return -1;
+		}
+	}
+	
+	private boolean findPath(Node root, int n, List<Integer> path)
+	{
 		
 	}
 
