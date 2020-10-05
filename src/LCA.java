22b8535af53038253642593f19a4c520643f140b
cartyad <cartyad@tcd.ie>
2020-10-05 20:21:09 +0100

    Update LCA.java
    Defined a new private method which will return the LCA node with the aforementioned inputs

--- src/LCA.java
+++ src/LCA.java
@@ -7,11 +7,16 @@ public class LCA {
 	private List<Integer> path1 = new ArrayList<>();
 	private List<Integer> path2 = new ArrayList<>();
 	
-	public int findLCA(int v, int w) {
-		
+	public int findLCA(int v, int w) 
+	{	
 		path1.clear();
 		path2.clear();
 		return findLCA(root, v, w);
+	}
+	
+	private int findLCA(Node root, int v, int w)
+	{
+		
 	}
 
 }
