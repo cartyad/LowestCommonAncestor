768ff3649ef385a32f39930972ba51c2cd5a8b6c
cartyad <cartyad@tcd.ie>
2020-10-05 20:08:08 +0100

    Update Node.java
    The initial code for my Node.java class implementation

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList; 
+import java.util.List;
 
 public class Node {
-
+	int data; 
+    Node left, right; 
+  
+    Node(int value) { 
+        data = value; 
+        left = right = null; 
+    } 
 }
