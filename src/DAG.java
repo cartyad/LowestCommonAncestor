4f8e61eeac482def9eaaca31ec63c9b4cff13444
cartyad <cartyad@tcd.ie>
2020-10-06 10:34:07 +0100

    Update DAG.java
    Wrote code for DAG Method whilst intantiating the V() method which returns #Vertives

--- src/DAG.java
+++ src/DAG.java
@@ -13,8 +13,29 @@ public class DAG {
 		{
 			throw new IllegalArgumentException("Number of vertices in the DAG must be greater than 0.");
 		}
-		
-		
+
+		else{
+			this.V = V;
+			this.E = 0;
+			//indegree = new boolean[V];
+			indegree = new int[V];
+			outdegree = new int[V];
+			visited = new int[V];
+			adj = new int[V][V];
+			for(int i = 0; i<V; i++)
+			{
+				for(int j=0;j<V;j++)
+				{
+					adj[i][j] = 0;
+				}
+			}
+		}
 	}
+	
+	public int V()
+	{
+        return V;
+    }
+	
 
 }
